7d354efeecf5e2d61e0c9a3b3ae8ef3fce395d76
Michael Belousov <michael.belousov98@gmail.com>
2023-10-03 21:50:34 -0400

    more scaffolding for dialogue ctx

---
+++ src/c_api.zig
@@ -0,0 +1,22 @@
+const DialogueContext = @import("./main.zig").DialogueContext;
+
+var malloc: ?*fn(c_uint) *anyopaque = null;
+var free: ?*fn(*anyopaque) void = null;
+
+export fn ade_set_alloc(
+  in_malloc: *fn(c_uint) *anyopaque,
+  in_free: *fn(*anyopaque) void,
+) void {
+  malloc = in_malloc;
+  free = in_free;
+}
+
+export fn ade_dialogue_ctx_create_json(json_ptr: [*]const u8, json_len: usize) ?*DialogueContext {
+  // FIXME: use a result type
+  if (malloc == null)
+  return null;
+}
+
+test "create context without allocator set fails" {
+  DialogueContext.
+}
--- src/main.zig
+++ src/main.zig
@@ -2,33 +2,114 @@ const std = @import("std");
 const json = std.json;
 const t = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const Index = usize;
 
-export fn alternis_set_allocators(
-    malloc: *fn(c_uint) *anyopaque,
-    free: *fn(*anyopaque) void,
-    realloc: *fn(*anyopaque, c_uint) *anyopaque
-) void {
-    _ = malloc;
-    _ = free;
-    _ = realloc;
-}
-
-const Variable = union (enum) {
-
+const Next = packed struct {
+  valid: u1 = false,
+  value: u31 = undefined,
 };
 
-export fn alternis_dlgctx_create_json(json: string) *DialogueContext {
-    _ = json;
-}
+const Node = packed union {
+  line: packed struct {
+    text: []const u8 = "",
+    // TODO: should this be optional?
+    metadata: []const u8 = "",
+    next: Next,
+  },
+  randomSwitch: packed struct {
+    nexts: []const Next, // does it make sense for these to be optional?
+    chances: []const u32,
+  },
+  reply: packed struct {
+    nexts: []const Next, // does it make sense for these to be optional?
+    /// utf8 assumed
+    texts: []const u8,
+  },
+  lock: packed struct {
+    booleanVariableIndex: Index,
+    next: Next,
+  },
+  unlock: packed struct {
+    booleanVariableIndex: Index,
+    next: Next,
+  },
+  call: packed struct {
+    functionIndex: Index,
+    next: Next,
+  },
+  goto: packed struct {
+    next: Next,
+  },
 
-export fn alternis_dlgctx_create_json(json: string) *DialogueContext {
-    _ = json;
-}
+  pub fn execute(self: @This(), ctx: *DialogueContext) void {
+    switch (self.data) {
+      .line => {},
+      .randomSwitch => {},
+      .reply => {},
+      .lock => {},
+      .unlock => {},
+      .call => {},
+      // does it make sense for self.next to be invalid?
+      .goto => { ctx.currentNode = self.next.value },
+    }
+  }
+};
 
+/// A function implemented by the environment
+const Callback = fn() void;
 
-test "basic add functionality" {
-    try t.expect(add(3, 7) == 10);
+/// The possible types for a variable
+const VariableType = enum {
+  /// also known as "text"
+  string,
+  /// also known as "true/false"
+  boolean,
+};
+
+pub const DialogueContext = struct {
+  entryNodeIndex: usize,
+  nodes: std.MultiArrayList(Node),
+  callbacks: []const Callback,
+  variables: struct {
+    /// has upper size bound of Index
+    strings: []u8,
+    /// has upper size bound of Index
+    booleans: []bool,
+  },
+
+  currentNode: usize,
+
+  pub fn initFromJson(json: []const u8, al: std.mem.Allocator) DialogueContext {
+    return .{
+      .nodes = std.MultiArrayList(Node){},
+      .callbacks = &.{},
+    };
+  }
+};
+
+// FIXME: I think it would be better to replace this with a custom
+// json parsing routine
+const DialogueJsonFormat = struct {
+  nodes: []const struct {
+    type: []const u8,
+    id: u64,
+  },
+  edges: []const []const u64,
+};
+
+test "create and run context to completion" {
+  // FIXME: load from tests/dir
+  const ctx_result = DialogueContext.initFromJson(
+    \\{
+    \\  "nodes": [
+    \\    {"type": "entry", "id": 0},
+    \\    {"type": "line", "id": 1}
+    \\  ],
+    \\  "edges": [
+    \\    [0, 1]
+    \\  ],
+    \\}
+  );
+
+  t.expect(ctx.result.is_ok());
 }
