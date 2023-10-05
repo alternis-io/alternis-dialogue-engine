c877105fa84cd608d6f11c18f5bf256aa376fe66
Michael Belousov <michael.belousov98@gmail.com>
2023-10-04 23:48:55 -0400

    move everything into lib and start root workspace

--- lib/src/c_api.zig
+++ lib/src/c_api.zig
@@ -4,20 +4,20 @@ var malloc: ?*fn(c_uint) *anyopaque = null;
 var free: ?*fn(*anyopaque) void = null;
 
 export fn ade_set_alloc(
-  in_malloc: *fn(c_uint) *anyopaque,
-  in_free: *fn(*anyopaque) void,
+    in_malloc: *fn(c_uint) *anyopaque,
+    in_free: *fn(*anyopaque) void,
 ) void {
-  malloc = in_malloc;
-  free = in_free;
+    malloc = in_malloc;
+    free = in_free;
 }
 
 export fn ade_dialogue_ctx_create_json(json_ptr: [*]const u8, json_len: usize) ?*DialogueContext {
-  // FIXME: use a result type
-  if (malloc == null)
-  return null;
+    // FIXME: use a result type
+    if (malloc == null)
+        return null;
 }
 
 test "create context without allocator set fails" {
-  DialogueContext.
+    DialogueContext.
 }
 
