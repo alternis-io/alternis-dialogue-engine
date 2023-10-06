1b8064494887ba6ad0a8f3bd2deba1ca354247e1
Michael Belousov <michael.belousov98@gmail.com>
2023-10-05 22:35:31 -0400

    convert test

--- js/tests/smoke.test.ts
+++ js/tests/smoke.test.ts
@@ -1,5 +1,59 @@
-import { DialogueContext } from "..";
+import { makeDialogueContext } from "..";
+import assert from "assert";
+
+// FIXME: load from alternis-wasm dependency
+const smallTestJson = {
+  "entryId": 0,
+  "nodes": [
+    {
+      "id": 0,
+      "line": {
+        "data": {
+          "speaker": "test",
+          "text": "hello world!"
+        },
+        "next": 1
+      }
+    },
+    {
+      "id": 1,
+      "line": {
+        "data": {
+          "speaker": "test",
+          "text": "goodbye cruel world!"
+        }
+      }
+    }
+  ]
+}
 
 describe("smoke", () => {
+  it("create and run context to completion", async () => {
+    const ctx = await makeDialogueContext(JSON.stringify(smallTestJson));
 
+    const step_result_1 = ctx.step();
+    assert.deepStrictEqual(step_result_1, {
+      line:  {
+        speaker: "test",
+        text: "hello world!",
+        metadata: undefined,
+      },
+    });
+
+    const step_result_2 = ctx.step();
+    assert.deepStrictEqual(step_result_2, {
+      line:  {
+        speaker: "test",
+        text: "goodbye cruel world!",
+        metadata: undefined,
+      },
+    });
+
+    const step_result_3 = ctx.step();
+    assert.deepStrictEqual(step_result_3, {
+      none: undefined,
+    });
+
+    ctx.dispose();
+  });
 });
