227770c7fffffe6f411f800c8c32598deea1a635
Michael Belousov <michael.belousov98@gmail.com>
2023-10-12 23:42:05 -0400

    add demo folder

--- plugins/godot/build.sh
+++ plugins/godot/build.sh
@@ -6,7 +6,11 @@ SCRIPT_DIR="$(dirname "$0")"
 mkdir -p $SCRIPT_DIR/thirdparty/godot
 pushd $SCRIPT_DIR/thirdparty/godot
 # generate json and headers
-$GODOT_BIN --dump-extension-api --dump-gd-extension-interface --headless
+$GODOT_BIN --dump-extension-api --dump-gdextension-interface --headless
 popd
 
+# build godot-cpp (c++ bindings) dependency
+pushd $SCRIPT_DIR/godot-cpp
+scons platform=linux -j8 custom_api_file=../thirdparty/godot/extension_api.json bits=64
+popd
 
